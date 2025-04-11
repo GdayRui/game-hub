7895ead5efcad6639a5e283218560326ec758828
GdayRui <lirui.jobs@gmail.com>
2025-04-11 17:30:36 +1000

    Build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,33 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+interface Props {
+  children: string;
+  limit: number;
+}
+
+const ExpandableText = ({ children, limit }: Props) => {
+  if (!children) return null;
+  if (children.length >= limit) return <Text>{children}</Text>;
+
+  const [expended, setExpended] = useState(false);
+  const description = expended
+    ? children
+    : children.substring(0, limit) + "...";
+  return (
+    <Text>
+      {description}
+      <Button
+        colorScheme="yellow"
+        marginLeft={1}
+        size="xs"
+        fontWeight="bold"
+        onClick={() => setExpended((pre) => !pre)}
+      >
+        {expended ? "Show less" : "Show more"}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
